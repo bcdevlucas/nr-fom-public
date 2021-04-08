7989fe41c6f3f81733b74654ecaa9b29049ad254
Ian Liu <po-yi.liu@cgi.com>
2021-04-08 13:16:54 -0700

    Upgrade to Angular 7: these are files changed by "ng update @angular/cli@7 @angular/core@7"

--- src/polyfills.ts
+++ src/polyfills.ts
@@ -42,10 +42,7 @@ import 'classlist.js'; // Run `npm install --save classlist.js`.
 
 /* ALL Firefox browsers require the following to support `@angular/animation`. */
 /** IE10 and IE11 requires the following to support `@angular/animation`. */
-import 'web-animations-js'; // Run `npm install --save web-animations-js`.
-
-/* Evergreen browsers require these. */
-import 'core-js/es7/reflect';
+import 'web-animations-js';
 import 'core-js/es7/array';
 
 /***************************************************************************************************
