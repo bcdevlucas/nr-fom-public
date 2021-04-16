be45611c902b87e06259da80998a37135e39f2b9
Basil Vandegriend <basil.vandegriend@cgi.com>
2021-04-16 17:31:04 -0600

    Fix dockerfile. Reorg OCP setup

--- openshift/create-deploy-dev.ps1
+++ openshift/create-deploy-dev.ps1
@@ -5,7 +5,7 @@ function CreatePublicFrontEnd {
     Write-Output "Create Public front-end for suffix $Suffix and env $Env using version $ApiVersion ..."
 
     Write-Output "Deleting existing resources..."
-    oc delete all,NetworkPolicy,ConfigMap -n a4b31c-$Env -l template=fom-public-deploy,tag=$Suffix
+    oc delete all,NetworkPolicy -n a4b31c-$Env -l template=fom-public-deploy,tag=$Suffix
 
     Write-Output "Creating public front-end..."
     oc process -f fom-public-deploy.yml -p ENV=$Env -p TAG=$Suffix -p HOSTNAME="nr-fom-public-$Suffix-$Env" -p IMAGE_STREAM_VERSION=$ApiVersion | oc create -n a4b31c-$Env -f -
